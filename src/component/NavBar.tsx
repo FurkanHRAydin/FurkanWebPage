63286c5cda5d48e34f73d3e382610cb77e085429
Furkan <furkan.h.aydin@hotmail.de>
2023-08-11 12:36:40 +0200

    Only refacotring left

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -55,19 +55,19 @@ const NavBar = () => {
                 </Link>
                 <div>
                     <ul style={{color: `${linkColor}`}} className="hidden md:flex mr-3 uppercase">
-                        <Link href="/">
+                        <Link href="/" scroll={false}>
                             <li className="ml-10 text-sm hover:border-b">Home</li>
                         </Link>
-                        <Link href="/#about">
+                        <Link href="/#about" scroll={false}>
                             <li className="ml-10 text-sm hover:border-b">About</li>
                         </Link>
-                        <Link href="/#skills">
+                        <Link href="/#skills" scroll={false}>
                             <li className="ml-10 text-sm hover:border-b">Skills</li>
                         </Link>
-                        <Link href="/#projects">
+                        <Link href="/#projects" scroll={false}>
                             <li className="ml-10 text-sm hover:border-b">Projects</li>
                         </Link>
-                        <Link href="/#contact">
+                        <Link href="/#contact" scroll={false}>
                             <li className="ml-10 text-sm hover:border-b">Contact</li>
                         </Link>
                     </ul>
@@ -103,19 +103,19 @@ const NavBar = () => {
                     </div>
                     <div className="py-4 flex flex-col">
                         <ul className="uppercase">
-                            <Link href="/">
+                            <Link href="/" scroll={false}>
                                 <li onClick={()=> setNav(false)} className="py-4 text-sm">Home</li>
                             </Link>
-                            <Link href="/#about">
+                            <Link href="/#about" scroll={false}>
                                 <li onClick={()=> setNav(false)} className="py-4 text-sm">About</li>
                             </Link>
-                            <Link href="/#skills">
+                            <Link href="/#skills" scroll={false}>
                                 <li onClick={()=> setNav(false)} className="py-4 text-sm">Skills</li>
                             </Link>
-                            <Link href="/#projects">
+                            <Link href="/#projects" scroll={false}>
                                 <li onClick={()=> setNav(false)} className="py-4 text-sm">Projects</li>
                             </Link>
-                            <Link href="/#contact">
+                            <Link href="/#contact" scroll={false}>
                                 <li onClick={()=> setNav(false)} className="py-4 text-sm">Contact</li>
                             </Link>
                         </ul>
--- src/component/projcets/Projects.tsx
+++ src/component/projcets/Projects.tsx
@@ -7,34 +7,39 @@ import {
 } from "@/const/project"
 
 const Projects = () => {
+
+
   return (
-    <div id="projects" className='w-full'>
-        <div className='max-2-[1240px] mx-auto px-2 py-16'>
-            <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
-            <h2 className='py-4'>What I&apos;ve Built</h2>
-            <div className='grid md:grid-cols-2 gap-8'>
-              <ProjectsCard
-                title={ChessScene.title}
-                img={ChessScene.imgUrl}
-                projectUrl="/projects/chessScene"
-                tech={ChessScene.technologyMain}
-              />
-              <ProjectsCard
-                title={MovieDatabase.title}
-                img={MovieDatabase.imgUrl}
-                projectUrl="/projects/movieDatabase"
-                tech={MovieDatabase.technologyMain}
-              />
-              <ProjectsCard
-                title={TheWorldHardestGame.title}
-                img={TheWorldHardestGame.imgUrl}
-                projectUrl="/projects/theWorldHardestGame"
-                tech={TheWorldHardestGame.technologyMain}
-              />
-            </div>
-        </div>
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+          <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+            Projects
+          </p>
+          <h2 className="py-4">What I&apos;ve Built</h2>
+          <div className="grid md:grid-cols-2 gap-8">
+            <ProjectsCard
+              title={ChessScene.title}
+              img={ChessScene.imgUrl}
+              projectUrl="/projects/chessScene"
+              tech={ChessScene.technologyMain}
+            />
+            <ProjectsCard
+              title={MovieDatabase.title}
+              img={MovieDatabase.imgUrl}
+              projectUrl="/projects/movieDatabase"
+              tech={MovieDatabase.technologyMain}
+            />
+            <ProjectsCard
+              title={TheWorldHardestGame.title}
+              img={TheWorldHardestGame.imgUrl}
+              projectUrl="/projects/theWorldHardestGame"
+              tech={TheWorldHardestGame.technologyMain}
+            />
+          </div>
+      </div>
     </div>
   );
 };
+
 
 export default Projects
